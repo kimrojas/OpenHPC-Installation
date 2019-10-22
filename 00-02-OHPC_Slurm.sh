b309fb223892c3dc3d78c3381c8561ef205eabfe
kimrojas <38098608+kimrojas@users.noreply.github.com>
2019-10-22 01:23:50 -0400

    Update 00-02-OHPC_Slurm.sh

--- 00-02-OHPC_Slurm.sh
+++ 00-02-OHPC_Slurm.sh
@@ -14,7 +14,7 @@ yum -y install ohpc-slurm-server | tee slurm_server_install.o
 perl -pi -e "s/ControlMachine=\S+/ControlMachine=master/" /etc/slurm/slurm.conf
 
 ## DEFINE INTERNAL INTERFACE (of master) ## 
-perl -pi -e "s/device = eth1/device = eno01/" /etc/warewulf/provision.conf
+perl -pi -e "s/device = eth1/device = eno1/" /etc/warewulf/provision.conf
 
 ## ENABLE tftp SERVICE ##
 perl -pi -e "s/^\s+disable\s+= yes/ disable = no/" /etc/xinetd.d/tftp
@@ -80,23 +80,134 @@ chroot $CHROOT systemctl enable ntpd
 echo "server 192.168.5.1" >> $CHROOT/etc/ntp.conf
 
 ## UPDATE SLURM CONFIGURATION ## (update for multiple types of computers)
+### MASTER
 perl -pi -e "s/ClusterName=\S+/ClusterName=HPCL/"  /etc/slurm/slurm.conf
 perl -pi -e "s/ControlMachine=\S+/ControlMachine=master/" /etc/slurm/slurm.conf
 
 perl -pi -e "s/^NodeName=\S+/NodeName=c[1-2]/" /etc/slurm/slurm.conf
 perl -pi -e "s/^PartitionName=normal Nodes=\S+/PartitionName=normal Nodes=c[1-2]/" /etc/slurm/slurm.conf
+perl -pi -e "s/Sockets=\S+/Sockets=1/" /etc/slurm/slurm.conf 
+perl -pi -e "s/CoresPerSocket=\S+/CoresPerSocket=4/" /etc/slurm/slurm.conf
+perl -pi -e "s/ThreadsPerCore=\S+/ThreadsPerCore=1/" /etc/slurm/slurm.conf
 
-# N/a
-perl -pi -e "s/^Sockets=\S+/Sockets=1/" /etc/slurm/slurm.conf 
+### NODE
+perl -pi -e "s/ClusterName=\S+/ClusterName=HPCL/"  $CHROOT/etc/slurm/slurm.conf
+perl -pi -e "s/ControlMachine=\S+/ControlMachine=master/" $CHROOT/etc/slurm/slurm.conf
 
-sed -i "s/ClusterName=\S+/ClusterName=${CLUSTER_NAME}/"  /etc/slurm/slurm.conf !!!!!!!!
-sed -i "s/ClusterName=\S+/ClusterName=myCLUSTER/"  /etc/slurm/slurm.conf!!
-sed -i "s/ControlMachine=\S+/ControlMachine=${MASTER_HOSTNAME}/" /etc/slurm/slurm.conf!!!
+perl -pi -e "s/^NodeName=\S+/NodeName=c[1-2]/" $CHROOT/etc/slurm/slurm.conf
+perl -pi -e "s/^PartitionName=normal Nodes=\S+/PartitionName=normal Nodes=c[1-2]/" $CHROOT/etc/slurm/slurm.conf
+perl -pi -e "s/Sockets=\S+/Sockets=1/" $CHROOT/etc/slurm/slurm.conf 
+perl -pi -e "s/CoresPerSocket=\S+/CoresPerSocket=4/" $CHROOT/etc/slurm/slurm.conf
+perl -pi -e "s/ThreadsPerCore=\S+/ThreadsPerCore=1/" $CHROOT/etc/slurm/slurm.conf
 
-perl -pi -e "s/^NodeName=\S+/NodeName=c[1-2]/" /etc/slurm/slurm.conf
-perl -pi -e "s/^PartitionName=normal Nodes=\S+/PartitionName=normal Nodes=c[1-2]/" /etc/slurm/slurm.conf
-perl -pi -e "s/^Sockets=\S+/Sockets=1/" /etc/slurm/slurm.conf
-perl -pi -e "s/^CoresPerSocket=\S+/CoresPerSocket=4/" /etc/slurm/slurm.conf
-perl -pi -e "s/^ThreadsPerCore=\S+/ThreadsPerCore=1/" /etc/slurm/slurm.conf
+## ENABLE MUNGE
+systemctl enable munge
+systemctl enable slurmctld
+systemctl start munge
+systemctl start slurmctld
+chroot $CHROOT systemctl enable slurmctld
 
+## DEFINE MEMLOCK
+perl -pi -e 's/# End of file/\* soft memlock unlimited\n$&/s' /etc/security/limits.conf
+perl -pi -e 's/# End of file/\* hard memlock unlimited\n$&/s' /etc/security/limits.conf
+perl -pi -e 's/# End of file/\* soft memlock unlimited\n$&/s' $CHROOT/etc/security/limits.conf 
+perl -pi -e 's/# End of file/\* hard memlock unlimited\n$&/s' $CHROOT/etc/security/limits.conf
 
+## DEFIN RSYSLOG FOR COMPUTE NODE BY POINT TO MASTER NODE
+perl -pi -e "s/\\#\\\$ModLoad imudp/\\\$ModLoad imudp/" /etc/rsyslog.conf
+perl -pi -e "s/\\#\\\$UDPServerRun 514/\\\$UDPServerRun 514/" /etc/rsyslog.conf 
+systemctl restart rsyslog
+echo "*.* @192.168.5.1:514" >> $CHROOT/etc/rsyslog.conf
+perl -pi -e "s/^\*\.info/\\#\*\.info/" $CHROOT/etc/rsyslog.conf
+perl -pi -e "s/^authpriv/\\#authpriv/" $CHROOT/etc/rsyslog.conf
+perl -pi -e "s/^mail/\\#mail/" $CHROOT/etc/rsyslog.conf
+perl -pi -e "s/^cron/\\#cron/" $CHROOT/etc/rsyslog.conf
+perl -pi -e "s/^uucp/\\#uucp/" $CHROOT/etc/rsyslog.conf
+
+## INSTALL GANGLIA
+yum -y install ohpc-ganglia
+yum -y --installroot=$CHROOT install ganglia-gmond-ohpc
+cp /opt/ohpc/pub/examples/ganglia/gmond.conf /etc/ganglia/gmond.conf 
+perl -pi -e "s/<sms>/master/" /etc/ganglia/gmond.conf
+cp /etc/ganglia/gmond.conf $CHROOT/etc/ganglia/gmond.conf 
+echo "gridname MySite.." >> /etc/ganglia/gmetad.conf
+systemctl enable gmond 
+systemctl enable gmetad 
+systemctl start gmond
+systemctl start gmetad
+chroot $CHROOT systemctl enable gmond 
+systemctl try-restart httpd
+
+## INSTALL CLUSTERSHELL
+yum -y install clustershell-ohpc
+cd /etc/clustershell/groups.d
+mv local.cfg local.cfg.orig
+echo "adm: master" > local.cfg
+echo "compute: c[1-2]" >> local.cfg
+echo "all: @adm,@compute" >> local.cfg
+
+## IMPORT FILE USING OpenHPC
+wwsh file import /etc/passwd 
+wwsh file import /etc/group
+wwsh file import /etc/shadow
+wwsh file import /etc/slurm/slurm.conf
+wwsh file import /etc/munge/munge.key
+wwsh file list
+
+## DEFINE BOOTSTRAP IMAGE
+export WW_CONF=/etc/warewulf/bootstrap.conf 
+echo "drivers += updates/kernel/" >> $WW_CONF 
+echo "drivers += overlay" >> $WW_CONF
+
+## SETUP BOOTSTRAP IMAGE
+wwbootstrap `uname -r`
+
+## CREATE VIRTUAL NODE FILE SYSTEM (VNFS) IMAGE
+wwvnfs --chroot $CHROOT
+
+## DEFINE GATEWAY
+echo "GATEWAYDEV=eno1" > /tmp/network.$$
+wwsh -y file import /tmp/network.$$ --name network
+wwsh -y file set network --path /etc/sysconfig/network --mode=0644 --uid=0 
+wwsh file list
+
+## REGISTER NODES
+wwsh -y node new c1 --ipaddr=192.168.5.51 --hwaddr=70:4d:7b:a0:f8:56 -D eno1
+wwsh -y node new c2 --ipaddr=192.168.5.52 --hwaddr=70:4d:7b:a0:fa:48 -D eno1np4s0
+wwsh node list
+
+## DEFINE VNFS FOR NODES
+wwsh -y provision set "c1" --vnfs=centos7 --bootstrap=`uname -r` --files=dynamic_hosts,passwd,group,shadow,slurm.conf,munge.key,network
+wwsh -y provision set "c2" --vnfs=centos7 --bootstrap=`uname -r` --files=dynamic_hosts,passwd,group,shadow,slurm.conf,munge.key,network
+wwsh provision list
+
+## RESTART GANGLIA
+systemctl restart gmond 
+systemctl restart gmetad 
+systemctl restart dhcpd 
+wwsh pxe update
+
+## Sync all file to compute node
+wwsh file resync
+
+## COMPUTE NODE INSTALLATION ( BOOT VIA PXE) 
+# Check run time by:
+pdsh -w c[1-2] uptime
+
+## RESOURCE MANAGER SETUP
+systemctl restart munge
+systemctl restart slurmctld
+pdsh -w c[1-2] systemctl restart slurmd
+
+## TEST MUNGE
+munge -n | unmunge
+munge -n | ssh c1 unmunge
+munge -n | ssh c2 unmunge
+
+## TEST SLURM
+systemctl status slurmctld
+ssh c1 systemctl status slurmd
+ssh c2 systemctl status slurmd
+
+## TEST RESOURCES
+scontrol show nodes
